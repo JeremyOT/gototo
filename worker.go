aa53ad7910467f1a9bef2835f13be91945babd83
JeremyOT <jeremyot@gmail.com>
2014-03-08 16:08:15 -0800

    update interface, remove IPC socket from standard impl

--- worker.go
+++ worker.go
@@ -1,5 +1,3 @@
-// +build !new
-
 package gototo
 
 import (
@@ -9,37 +7,7 @@ import (
 	"runtime"
 )
 
-var registeredWorkerFunctions map[string]WorkerFunction = make(map[string]WorkerFunction)
-
-type WorkerCommand int
-
-const (
-	QUIT = WorkerCommand(-1)
-)
-
-type WorkerStatus int
-
-const (
-	STOPPED = WorkerStatus(0)
-	RUNNING = WorkerStatus(1)
-)
-
-var logger *log.Logger
-
-func SetLogger(l *log.Logger) {
-	logger = l
-}
-
-func writeLog(message ...interface{}) {
-	if logger != nil {
-		logger.Println(message)
-	} else {
-		println(message)
-	}
-}
-
-type WorkerFunction func(interface{}, func(interface{}))
-
+// Run a ZMQ router->dealer at the specified addresses.
 func RunRouter(routerAddress, dealerAddress string, routerBind, dealerBind bool) error {
 	context, _ := zmq.NewContext()
 	defer context.Close()
@@ -60,74 +28,125 @@ func RunRouter(routerAddress, dealerAddress string, routerBind, dealerBind bool)
 	return zmq.Device(zmq.QUEUE, router, dealer)
 }
 
-func RegisterWorkerFunction(name string, workerFunction WorkerFunction) {
-	registeredWorkerFunctions[name] = workerFunction
+type WorkerFunction func(interface{}) interface{}
+
+type Worker struct {
+	logger                    *log.Logger
+	registeredWorkerFunctions map[string]WorkerFunction
+	activeTimeout             int
+	passiveTimeout            int
+	quit                      chan int
+	wait                      chan int
+	address                   string
+	maxWorkers                int
+	runningWorkers            int
 }
 
-func RunWorker(address string, control chan WorkerCommand, status chan WorkerStatus) {
-	defer func() { status <- STOPPED }()
+// Create a new worker bound to address that will run at most count functions at a time.
+// If count == 0, it will default to runtime.NumCPU().
+func New(address string, count int) *Worker {
+	if count == 0 {
+		count = runtime.NumCPU()
+	}
+	return &Worker{registeredWorkerFunctions: make(map[string]WorkerFunction),
+		activeTimeout:  1,
+		passiveTimeout: 100,
+		quit:           make(chan int),
+		wait:           make(chan int),
+		maxWorkers:     count,
+		address:        address,
+	}
+}
+
+func (w *Worker) Quit() {
+	w.quit <- 1
+}
+
+func (w *Worker) Wait() {
+	<-w.wait
+}
+
+func (w *Worker) SetLogger(l *log.Logger) {
+	w.logger = l
+}
+
+func (w *Worker) writeLog(message ...interface{}) {
+	if w.logger != nil {
+		w.logger.Println(message)
+	} else {
+		log.Println(message)
+	}
+}
+
+func (w *Worker) RegisterWorkerFunction(name string, workerFunction WorkerFunction) {
+	w.registeredWorkerFunctions[name] = workerFunction
+}
+
+// Run a worker function and send the response to responseChannel
+func runFunction(responseChannel chan [][]byte, message [][]byte, parameters interface{}, workerFunction WorkerFunction) {
+	response := workerFunction(parameters)
+	responseData, _ := json.Marshal(response)
+	message[len(message)-1] = responseData
+	responseChannel <- message
+}
+
+func (w *Worker) Run() {
+	defer func() { close(w.wait) }()
 	context, _ := zmq.NewContext()
 	defer context.Close()
-	socket, _ := context.NewSocket(zmq.REP)
+	socket, _ := context.NewSocket(zmq.ROUTER)
 	defer socket.Close()
-	socket.SetSockOptInt(zmq.RCVTIMEO, 100)
-	socket.Connect(address)
-	var responseChannel = make(chan []byte)
-	status <- RUNNING
+	socket.SetSockOptInt(zmq.RCVTIMEO, w.passiveTimeout)
+	socket.Bind(w.address)
+	w.runningWorkers = 0
+	responseChannel := make(chan [][]byte)
+	sendResponse := func(response [][]byte) {
+		w.runningWorkers -= 1
+		socket.SendMultipart(response, 0)
+		if w.runningWorkers == 0 {
+			socket.SetSockOptInt(zmq.RCVTIMEO, w.passiveTimeout)
+		}
+	}
 	for {
+		if w.runningWorkers == w.maxWorkers {
+			select {
+			case response := <-responseChannel:
+				sendResponse(response)
+			case <-w.quit:
+				return
+			}
+		}
 		select {
-		case <-control:
+		case <-w.quit:
 			return
+		case response := <-responseChannel:
+			sendResponse(response)
+			break
 		default:
 			message, err := socket.RecvMultipart(0)
 			if err != nil {
+				// Needed to yield to goroutines when GOMAXPROCS is 1.
+				// Note: The 1.2 preemptive scheduler doesn't seem to work here,
+				// so this is still required.
+				runtime.Gosched()
 				break
 			}
 			data := map[string]interface{}{}
-			callback := func(response interface{}) {
-				responseMessage, _ := json.Marshal(response)
-				responseChannel <- responseMessage
-			}
 			json.Unmarshal(message[len(message)-1], &data)
 			if data == nil {
-				writeLog("Received invalid message")
-				continue
+				w.writeLog("Received invalid message")
+				break
 			}
-			workerFunction := registeredWorkerFunctions[data["method"].(string)]
+			workerFunction := w.registeredWorkerFunctions[data["method"].(string)]
 			if workerFunction == nil {
-				writeLog("Unregistered worker function:", data["method"].(string))
-				continue
+				w.writeLog("Unregistered worker function:", data["method"].(string))
+				break
 			}
-			go workerFunction(data["parameters"], callback)
-			response := <-responseChannel
-			message[len(message)-1] = response
-			socket.SendMultipart(message, 0)
+			if w.runningWorkers == 0 {
+				socket.SetSockOptInt(zmq.RCVTIMEO, w.activeTimeout)
+			}
+			w.runningWorkers += 1
+			go runFunction(responseChannel, message, data["parameters"], workerFunction)
 		}
-	}
-}
-
-func StartWorker(address string) (WorkerStatus, chan WorkerCommand, chan WorkerStatus) {
-	controlChannel := make(chan WorkerCommand)
-	statusChannel := make(chan WorkerStatus)
-	go RunWorker(address, controlChannel, statusChannel)
-	return <-statusChannel, controlChannel, statusChannel
-}
-
-func RunWorkerServer(routerAddress, internalAddress string, routerBind bool, count int) {
-	go RunRouter(routerAddress, internalAddress, routerBind, true)
-	RunWorkers(internalAddress, count)
-}
-
-func RunWorkers(address string, count int) {
-	if count <= 0 {
-		count = runtime.NumCPU()
-	}
-	statusChannels := make([]chan WorkerStatus, count)
-	for i := 0; i < count; i++ {
-		_, _, statusChannel := StartWorker(address)
-		statusChannels[i] = statusChannel
-	}
-	for _, c := range statusChannels {
-		<-c
 	}
 }
